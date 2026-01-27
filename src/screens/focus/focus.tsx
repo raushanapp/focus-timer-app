e50cbee55e385eb2ec1204409bb5a011a8104c36
Raushan Kumar <sraushan890@gamil.com>
2026-01-27 23:20:33 +0530

    Remove unnecessary whitespace in Focus screen component

--- src/screens/focus/focus.tsx
+++ src/screens/focus/focus.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import { View, Text } from 'react-native';
 import GradientBackground from '@/components/gradient-background-wrapper-component';
 import FocusTextInput from '@/components/focus-text-input';
